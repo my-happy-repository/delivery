1ec6023d60393edddd40869a606b40cf23a3f340
behappyleee <72903623+behappyleee@users.noreply.github.com>
2024-10-07 23:55:29 +0900

    studying commit

--- chatting/src/main/kotlin/com/study/chatting/controller/dto/ChattingMessage.kt
+++ chatting/src/main/kotlin/com/study/chatting/controller/dto/ChattingMessage.kt
@@ -1,5 +1,8 @@
 package com.study.chatting.controller.dto
 
+import com.fasterxml.jackson.annotation.JsonProperty
+
 data class ChattingMessage(
+    @JsonProperty("message")
     val message: String,
 )
