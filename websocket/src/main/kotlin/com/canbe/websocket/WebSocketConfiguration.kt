fce54403400a9de2477d2557f3df17de4e86dc4a
behappyleee <72903623+behappyleee@users.noreply.github.com>
2024-10-14 23:59:28 +0900

    studying commit

--- websocket/src/main/kotlin/com/canbe/websocket/WebSocketConfiguration.kt
+++ websocket/src/main/kotlin/com/canbe/websocket/WebSocketConfiguration.kt
@@ -1,5 +1,6 @@
 package com.canbe.websocket
 
+import org.slf4j.LoggerFactory
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.socket.WebSocketHandler
@@ -10,7 +11,10 @@ import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry
 @Configuration
 @EnableWebSocket
 class WebSocketConfiguration: WebSocketConfigurer {
+    val log = LoggerFactory.getLogger(WebSocketConfiguration::class.java)
+
     override fun registerWebSocketHandlers(registry: WebSocketHandlerRegistry) {
+        log.info("")
         registry.addHandler(WebSocketHandler(), "/hello")
             .setAllowedOrigins("*")
     }
